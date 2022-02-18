16a0d50506afd13d7b0709610f05b46cc9b83ec2
Adam Bukowski <bukowski.adam@gmail.com>
2022-02-18 15:05:31 +0100

    feat: add package with working command

---
+++ README.md
@@ -0,0 +1,14 @@
+# Git Checkout In-Progress
+
+## Configuration
+
+Set those environment variables
+
+```
+GIT_CIP_GITHUB_TOKEN=<your personal access token>
+GIT_CIP_GITHUB_API_URL=https://api.github.com
+GIT_CIP_ZENHUB_TOKEN=<your zenhub token>
+GIT_CIP_ZENHUB_API_URL=https://api.zenhub.com
+GIT_CIP_ZENHUB_IN_PROGRESS_PIPELINE=in progress
+GIT_CIP_TEMPLATE=joe-doe/{number}/{title}
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+import got from "got";
+import { execSync } from "child_process";
+
+const {
+  GIT_CIP_GITHUB_TOKEN: githubToken,
+  GIT_CIP_GITHUB_API_URL: githubApiUrl,
+  GIT_CIP_ZENHUB_TOKEN: zenhubToken,
+  GIT_CIP_ZENHUB_API_URL: zenhubApiUrl,
+  GIT_CIP_ZENHUB_IN_PROGRESS_PIPELINE: inProgressPipelineName,
+  GIT_CIP_TEMPLATE: branchNameTemplate,
+} = process.env;
+
+const missingEnvs = [
+  "GIT_CIP_GITHUB_TOKEN",
+  "GIT_CIP_GITHUB_API_URL",
+  "GIT_CIP_ZENHUB_TOKEN",
+  "GIT_CIP_ZENHUB_API_URL",
+  "GIT_CIP_ZENHUB_IN_PROGRESS_PIPELINE",
+  "GIT_CIP_TEMPLATE",
+].filter((env) => !process.env[env]);
+
+if (missingEnvs.length) {
+  panic(
+    `Configuration environment variables missing:\n - ${missingEnvs.join(
+      "\n - "
+    )}`
+  );
+}
+
+function panic(message) {
+  console.error(message);
+  process.exit(1);
+}
+
+async function getIssuesInProgress() {
+  let openIssues;
+  let zenhubBoard;
+
+  try {
+    openIssues = await got(`${githubApiUrl}/v3/issues`, {
+      headers: {
+        accept: "application/vnd.github.inertia-preview+json",
+        authorization: `token ${githubToken}`,
+      },
+    }).json();
+  } catch (error) {
+    panic("Error fetching github issues: ", error.toString());
+  }
+
+  try {
+    zenhubBoard = await got(`${zenhubApiUrl}/p1/repositories/39456/board`, {
+      headers: {
+        "X-Authentication-Token": zenhubToken,
+      },
+    }).json();
+  } catch (error) {
+    panic("Error fetching zenhub board: ", error.toString());
+  }
+
+  const inProgressPipeline = zenhubBoard.pipelines.find(
+    (pipeline) => pipeline.name.toLowerCase() === inProgressPipelineName
+  );
+  const issuesInProgress = inProgressPipeline.issues
+    .map((issue) =>
+      openIssues.find((openIssue) => openIssue.number === issue.issue_number)
+    )
+    .filter(Boolean);
+
+  return issuesInProgress;
+}
+
+function createGitBranch(branchName) {
+  const branchExists = execSync(`git branch -l "${branchName}"`)?.toString();
+  if (!branchExists) {
+    execSync(`git branch "${branchName}"`);
+    console.log(`Created branch: ${branchName}`);
+  }
+  execSync(`git checkout "${branchName}"`);
+}
+
+function createTitle(title) {
+  return title.toLowerCase().replace(/[\s~^:./\\]/g, "-");
+}
+
+async function run() {
+  const issuesInProgress = await getIssuesInProgress();
+  const selectedIssue = issuesInProgress[0];
+  const branchName = branchNameTemplate
+    .replace("{number}", selectedIssue.number)
+    .replace("{title}", createTitle(selectedIssue.title));
+
+  createGitBranch(branchName);
+}
+
+run();
