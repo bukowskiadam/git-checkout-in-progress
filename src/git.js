286a315a73ea5f07b41cf9202fd11e2e2e4db516
Adam Bukowski <bukowski.adam@gmail.com>
2022-03-24 16:23:20 +0100

    fix: make it node 12 compatible

--- src/git.js
+++ src/git.js
@@ -1,10 +1,18 @@
 import { execSync } from "node:child_process";
 
+function bufferHasTextContent(buffer) {
+  return ("" + (buffer || "")).trim().length > 0;
+}
+
 export function createGitBranch(branchName) {
-  const branchExists = execSync(`git branch -l "${branchName}"`)?.toString();
+  const branchExists = bufferHasTextContent(
+    execSync(`git branch -l "${branchName}"`)
+  );
+
   if (!branchExists) {
     execSync(`git branch "${branchName}"`);
     console.log(`Created branch: ${branchName}`);
   }
+
   execSync(`git checkout "${branchName}"`);
 }
